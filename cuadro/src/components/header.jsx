f9a6685fd21956ed0e06be9c89ab531debb5f33f
Karen-Hoil <karenhoil2004@gmail.com>
2023-10-30 12:15:39 -0500

    login

--- cuadro/src/components/header.jsx
+++ cuadro/src/components/header.jsx
@@ -6,8 +6,7 @@ function Header(){
         <nav className="navbar bg-body-tertiary">
   <div className="container-fluid ">
     <div className="nav_titulo">
-    <img src="https://cdn.icon-icons.com/icons2/2279/PNG/512/examination_health_exam_care_medical_awareness_treatment_icon_140899.png"/>
-    <h1>Control de medicamentos</h1>
+    <h1>Bienvenido Usuario</h1>
     </div>
   </div>
 </nav>
--- cuadro/src/page/login.jsx
+++ cuadro/src/page/login.jsx
@@ -3,31 +3,32 @@ import React from 'react'
 function Login() {
   return (
     <>
+    <div className='body'>
   <div className="main">
     <input type="checkbox" id="chk" aria-hidden="true" />
     <div className="signup">
       <form>
         <label htmlFor="chk" aria-hidden="true">
-          Sign up
+          Registrate
         </label>
-        <input type="text" name="txt" placeholder="User name" required="" />
-        <input type="email" name="email" placeholder="Email" required="" />
-        <input type="password" name="pswd" placeholder="Password" required="" />
-        <button>Sign up</button>
+        <input type="text" name="txt" placeholder="Usuario" required="" />
+        <input type="email" name="email" placeholder="Correo" required="" />
+        <input type="password" name="pswd" placeholder="Contraseña" required="" />
+        <button>Registrate</button>
       </form>
     </div>
     <div className="login">
       <form>
         <label htmlFor="chk" aria-hidden="true">
-          Login
+          Inicio de sesión
         </label>
-        <input type="email" name="email" placeholder="Email" required="" />
-        <input type="password" name="pswd" placeholder="Password" required="" />
-        <button>Login</button>
+        <input type="email" name="email" placeholder="Correo" required="" />
+        <input type="password" name="pswd" placeholder="Contraseña" required="" />
+        <button>Inicio de sesión</button>
       </form>
     </div>
   </div>
-
+  </div>
     </>
   )
 }
